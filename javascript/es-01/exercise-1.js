8f11a2408415d6d0f4870b313c42f5bebc948cba
Janmanpreet Singh <106154803+Erodot0@users.noreply.github.com>
2023-07-07 21:58:17 +0200

    javascript commit

---
+++ javascript/es-01/exercise-1.js
@@ -0,0 +1,19 @@
+function canPlay() {
+  let personName = 'Paul';
+
+personName += ' plays football'; 
+
+console.log(personName);
+}
+
+canPlay();
+
+/* 
+Alternativa
+
+let personName = "Paul";
+
+  personName += ' plays football';
+
+  console.log(personName);
+  */
